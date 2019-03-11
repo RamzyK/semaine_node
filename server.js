9c27e8724b7f00866bcbb6eaa14802f579954b31
Ramzy <gkermad.ramzy@gmail.com>
2019-03-11 15:03:21 +0100

    adding localisation & meteo

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
-
 var app = express();
 
 app.get("/hello", (req, res) => {
   res.send("Hello World");
+})
+
+app.post("/chat", (req, res) => {
+  const msg = req.msg;
+  if(msg != "ville"){
+    res.send("Nous sommes à Paris");
+  }else if(msg != "météo"){
+    res.send("Il fait beau")
+  }
 })
 
 app.listen(port, () => {
